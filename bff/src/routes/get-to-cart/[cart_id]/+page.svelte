a91aba49143875f60916f8518d6760ea563201cc
Yano Daisuke (矢野) <dyano@labellevie.jp>
2025-04-16 12:32:08 +0900

    スタイル追加

--- bff/src/routes/get-to-cart/[cart_id]/+page.svelte
+++ bff/src/routes/get-to-cart/[cart_id]/+page.svelte
@@ -7,28 +7,32 @@
 
 <h1 class="text-xl">Get Cart Page</h1>
 
-<table>
+<table class="border">
   <thead>
-    <tr>
-      <th>id</th>
-      <th>userUuid</th>
-      <th>cartCode</th>
-      <th>cartItems</th>
+    <tr class="border">
+      <th class="border p-4">id</th>
+      <th class="border p-4">userUuid</th>
+      <th class="border p-4">cartCode</th>
+      <th class="border p-4">cartItems</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>{cart.id}</td>
-      <td>{cart.userUuid}</td>
-      <td>{cart.cartCode}</td>
-      <td>
-        <ul>
-          <li>id: {cart.cartItems[0].id}</li>
-          <li>skuCode: {cart.cartItems[0].skuCode}</li>
-          <li>price: {cart.cartItems[0].price}</li>
-          <li>quantity: {cart.cartItems[0].quantity}</li>
-          <li>cartId: {cart.cartItems[0].cartId}</li>
-        </ul>
+    <tr class="border">
+      <td class="border p-4">{cart.id}</td>
+      <td class="border p-4">{cart.userUuid}</td>
+      <td class="border p-4">{cart.cartCode}</td>
+      <td class="border p-4">
+        {#each cart.cartItems as cartItems}
+          <div class="flex flex-col gap-4">
+            <ul class="bg-gray-100 p-4">
+              <li>id: {cartItems.id}</li>
+              <li>skuCode: {cartItems.skuCode}</li>
+              <li>price: {cartItems.price}</li>
+              <li>quantity: {cartItems.quantity}</li>
+              <li>cartId: {cartItems.cartId}</li>
+            </ul>
+          </div>
+        {/each}
       </td>
     </tr>
   </tbody>
